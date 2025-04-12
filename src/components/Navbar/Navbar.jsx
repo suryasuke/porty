f0a674cdae47fb5bc88a24f0645df262dd7f7921
SURYA S <138099237+suryasuke@users.noreply.github.com>
2025-04-12 08:24:47 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,41 @@
 
+import React, { useState } from 'react';
+import '../Navbar/Navbar.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logof.png';
+
+function Navbar(props) {
+  const { currentPath } = props;
+  const Menu = currentPath;
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img id='imgslogo' src={logo} alt='logo.png' />
+      </div>
+
+      <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <ul className={`nav-menu ${menuOpen ? 'mobile' : ''}`}>
+        <li><Link to='/' onClick={()=>setMenuOpen(false)} style={{ textDecoration: 'none', color: Menu === '/' ? '#e95f35' : 'inherit' }}>Overview</Link></li>
+        <li><Link to='/projects' onClick={()=>setMenuOpen(false)}style={{ textDecoration: 'none', color: Menu === '/projects' ? '#e95f35' : 'inherit' }}>Projects</Link></li>
+        <li><Link to='/activity' onClick={()=>setMenuOpen(false)} style={{ textDecoration: 'none', color: Menu === '/activity' ? '#e95f35' : 'inherit' }}>Activity</Link></li>
+        <li><Link to='/feedback' onClick={()=>setMenuOpen(false)} style={{ textDecoration: 'none', color: Menu === '/feedback' ? '#e95f35' : 'inherit' }}>Others</Link></li>
+      </ul>
+
+      <div className='contact-btn'>
+        <a href='https://www.instagram.com/surya_____sol/'><button className='button-org'>Fast Contact</button></a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
