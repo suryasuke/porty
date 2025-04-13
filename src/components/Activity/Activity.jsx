221ad1fefe62cc34d204eeda3df25246c9d7f771
SURYA S <138099237+suryasuke@users.noreply.github.com>
2025-04-13 22:07:29 +0530

    Update Activity.jsx

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -12,7 +12,7 @@ import png8 from '../../assets/8.png' ;
 
 function Activity() {
   return (
-    <div>
+    <div className='conatin'>
       <div className='contain-dot'>
 
          <div className='dot'></div>
@@ -35,7 +35,7 @@ function Activity() {
               
             </div>
             <div className='container-time right-container'>
-            <p className='icons'><img src={png2} style={{position :'absolute' , right :'94.5%'}} alt='2 img'/></p>
+            <p className='icons hi-img'><img src={png2} alt='2 img'/></p>
               <div className='text-box'>
                 <div className='tb'>
                 <h1>Improve My Skills in Business</h1>
@@ -52,7 +52,7 @@ function Activity() {
             <p className='icons'><img src={png3} alt='3 img'/></p>
               <div className='text-box'>
               <div className='tb'>
-                <h1>Learned Python Fundamentals</h1>
+                <h1>Learned Python</h1>
                  <small><i style={{fontSize :'13px'}}>2021 - 2022</i></small>
                  <p>During this period, I focused on developing my business acumen by studying key business concepts and analyzing real-world scenarios to enhance my decision-making abilities. My approach involved not only understanding theoretical frameworks but also applying them in practical, efficient ways to solve business problems</p>
 
@@ -62,7 +62,7 @@ function Activity() {
               
             </div>
             <div className='container-time right-container'>
-            <p className='icons'><img src={png4} style={{position :'absolute' , right :'94.5%'}} alt='4 img'/></p>
+            <p className='icons  hi-img'><img src={png4}  alt='4 img'/></p>
               <div className='text-box'>
               <div className='tb'>
                 <h1>Started MERN Stack Course</h1>
@@ -88,7 +88,7 @@ function Activity() {
               
             </div>
             <div className='container-time right-container'>
-            <p className='icons'><img src={png6} style={{position :'absolute' , right :'94.5%'}} alt='6 img'/></p>
+            <p className='icons  hi-img'><img src={png6} style={{position :'absolute' , right :'94.5%'}} alt='6 img'/></p>
               <div className='text-box'>
               <div className='tb'>
                 <h1>Done my first Internship at Parts2Products(thirunelveli)</h1>
@@ -115,7 +115,7 @@ function Activity() {
               
             </div>
             <div className='container-time right-container'>
-            <p className='icons'><img src={png8} style={{position :'absolute' , right :'94.5%'}} alt='6 img'/></p>
+            <p className='icons hi-img'><img src={png8}  alt='6 img'/></p>
               <div className='text-box'>
               <div className='tb'>
                 <h1>Planned to do PG in Canada (MBA)</h1>
@@ -135,3 +135,4 @@ function Activity() {
 }
 
 export default Activity ;
+
